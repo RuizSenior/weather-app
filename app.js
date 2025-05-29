c5aa492de2c0c2899d81a3313c6aaeaec71840ef
Sebastian Ruiz <sebasrhrh@gmail.com>
2025-05-29 00:44:15 -0500

    fixes

--- app.js
+++ app.js
@@ -10,16 +10,47 @@ async function getWeather() {
     const city = document.getElementById('cityInput').value;
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await response.json();
-    console.log(data);
+
+    const iconCode = data.weather[0].icon;
+
+    const iconEmojiMap = {
+      "01d": "☀️",    
+      "01n": "🌙",    
+      "02d": "🌤️",   
+      "02n": "🌤️",  
+      "03d": "☁️",    
+      "03n": "☁️",
+      "04d": "☁️",   
+      "04n": "☁️",
+      "09d": "🌧️",  
+      "09n": "🌧️",
+      "10d": "🌦️",   
+      "10n": "🌧️",   
+      "11d": "⛈️",   
+      "11n": "⛈️",
+      "13d": "❄️",   
+      "13n": "❄️",
+      "50d": "🌫️",   
+      "50n": "🌫️"
+    };
+
+    const emoji = iconEmojiMap[iconCode] || "🌈";
 
     weatherDiv.innerHTML = `
-      <h2>${data.name}, ${data.sys.country}</h2>
-      <p><strong>Temp:</strong> ${data.main.temp} °C</p>
-      <p><strong>Weather:</strong> ${data.weather[0].main}, ${data.weather[0].description}</p>
-      <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
+      <div id='weatherContent' style="display:flex;align-items:center;">
+        <div style="font-size:10rem;">${emoji}</div>
+        <div style="text-align:left; margin-left:20px;">
+          <h2>${data.name}, ${data.sys.country}</h2>
+          <p><strong>Temp:</strong> ${data.main.temp} °C</p>
+          <p><strong>Feels like:</strong> ${data.main.feels_like} °C</p>
+          <p><strong>Weather:</strong> ${data.weather[0].main}, ${data.weather[0].description}</p>
+          <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
+
+        </div>
+      </div>
     `;
   } catch (error) {
     console.error('Error fetching weather data:', error);
-    weatherDiv.innerHTML = `<p style="color:red;">No se pudo obtener el clima. Intenta de nuevo.</p>`;
+    weatherDiv.innerHTML = `<p style="color:red;">Could not get weather. Please try again.</p>`;
   }
 }
